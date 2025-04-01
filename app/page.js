861f6839524610d1ad25f46a315c9ea0dd6923ff
Ashish <kapoorashish713@gmail.com>
2025-04-01 21:16:31 +0530

    new

--- app/page.js
+++ app/page.js
@@ -5,14 +5,14 @@ import Cards from "@/components/Cards/Cards1";
 export default function Home() {
   return (
     <>
-      <main className="bg-1 backdrop-opacity-25 min-w-screen min-h-screen flex justify-center items-center">
+      <main className="bg-1 backdrop-opacity-25 min-w-screen min-h-screen flex justify-center items-center overflow-hidden">
         <div className="main bg-[#1d1834c1] text-white flex flex-col gap-4 min-w-screen min-h-screen justify-center p-10 md:p-20">
           <h1 className="md:text-[45px] font-semibold">{Data.Home}</h1>
-          <p className="w-120 text-[14px]">{Data.Homedis}</p>
+          <p className="md:w-120 text-[14px]">{Data.Homedis}</p>
           <Button1 dis="Admission now" />
         </div>
       </main>
-      <main className="min-w-screen min-h-screen flex flex-col items-center">
+      <main className="min-w-screen min-h-screen flex flex-col items-center overflow-hidden">
         <div className="w-full h-full absolute flex flex-col -z-20">
           <div className="w-full h-120 bg-[#7971EA]"></div>
           <div className="w-full h-full"> </div>{" "}
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,7 +3,10 @@ import NavLg from "./nav/NavLg";
 export default function Navbar(){
   return(
     <>
+    <div className="md:block hidden">
     <NavLg/>
+    </div>
+    
     </>
   )
 }
